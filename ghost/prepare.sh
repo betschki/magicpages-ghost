78063c48f9082eafbf9e338b64a494b65960fc61
Jannis Fedoruk-Betschki <github@betschki.com>
2023-03-09 18:50:32 +0000

    it actually works now

--- ghost/prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Adding posts data..."
-
-mysql -h $database__connection__host -P 3306 -u $database__connection__user -p$database__connection__password $database__connection__database < ./posts.sql
-
-echo "Posts added successfully!"
